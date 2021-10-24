7a6e9836e659827f86f3fc6cf5ed5b5ba8223abc
frsoto <frsoto@gmail.com>
2021-10-24 18:45:45 -0300

    20211024-Creo primeras clases

--- chapter_12/alien_invasion.py
+++ chapter_12/alien_invasion.py
@@ -1,31 +1,55 @@
+# import the sys and pygame modules
+import sys
 import pygame
-from pygame.sprite import Group
-
 from settings import Settings
-from ship import Ship
-import game_functions as gf
 
-def run_game():
-    # Initialize pygame, settings, and screen object.
-    pygame.init()
-    ai_settings = Settings()
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
-    pygame.display.set_caption("Alien Invasion")
-    
-    # Set the background color.
-    bg_color = (230, 230, 230)
-    
-    # Make a ship.
-    ship = Ship(ai_settings, screen)
-    # Make a group to store bullets in.
-    bullets = Group()
 
-    # Start the main loop for the game.
-    while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init() 
+        # function initializes the background settings that Pygame needs to 
+        # work properly
+        
+        self.settings = Settings()
+        
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        # crea ventana con esa resolucion. surface
+        # se asigna a un atributo para desp usarlo.
+        
+        pygame.display.set_caption("Alien Invasion Fernando")
+        # titulo ventana
+        
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
 
-run_game()
+    # Metotodo:
+    def run_game(self): #controla el juego
+        """Start the main loop for the game."""
+        while True: #corre continuamente
+            # Watch for keyboard and mouse events. An event is an action that 
+            # the user performs while playing the game, such as pressing a key
+            # or moving the mouse
+            for event in pygame.event.get(): 
+            # To access the events that Pygame detects, we’ll use the 
+            # pygame.event.get() function. This function returns a list of 
+            #  events that have taken place since the last time this function 
+            # was called. Inside the loop, we’ll write a series of if
+            #  statements to detect and respond to specific events. ejemplo 
+            #  click en la x de la ventana
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
+            
+            # Make the most recently drawn screen visible. refresca la pant.
+            pygame.display.flip()
+
+# instanciamos
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
