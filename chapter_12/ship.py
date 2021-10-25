4188227e6db7a13bf177cfdfe86c1bf7ba3a0112
frsoto <frsoto@gmail.com>
2021-10-25 20:58:16 -0300

    202110225

--- chapter_12/ship.py
+++ chapter_12/ship.py
@@ -1,39 +1,31 @@
 import pygame
 
-class Ship():
+class Ship:
+    """A class to manage the ship."""
+    
+    def __init__(self, ai_game):
+        # ai_game is a reference to the current instance of the AlienInvasion class
+        """Initialize the ship and set its starting position."""
+        self.screen = ai_game.screen
+        # we assign the screen to an attribute of Ship, so we can access it 
+        # easily in all the methods in this class
+        
+        self.screen_rect = ai_game.screen.get_rect()
+        # we access the screen’s rect attribute using the get_rect() method
+        # and assign it to self.screen_rect. Doing so allows us to place the 
+        # ship in the correct location on the screen
+    
 
-    def __init__(self, ai_settings, screen):
-        """Initialize the ship, and set its starting position."""
-        self.screen = screen
-        self.ai_settings = ai_settings
-
-        # Load the ship image, and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
+        # carga la imagen
+        
         self.rect = self.image.get_rect()
-        self.screen_rect = screen.get_rect()
-
-        # Start each new ship at the bottom center of the screen.
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
         
-        # Store a decimal value for the ship's center.
-        self.center = float(self.rect.centerx)
-        
-        # Movement flags.
-        self.moving_right = False
-        self.moving_left = False
-        
-    def update(self):
-        """Update the ship's position, based on movement flags."""
-        # Update the ship's center value, not the rect.
-        if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left and self.rect.left > 0:
-            self.center -= self.ai_settings.ship_speed_factor
             
-        # Update rect object from self.center.
-        self.rect.centerx = self.center
-
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+    
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+    
--- chapter_15/scatter_squares.py
+++ chapter_15/scatter_squares.py
@@ -1,19 +1,48 @@
 import matplotlib.pyplot as plt
 
-x_values = list(range(1, 1001))
+
+# x_values = [1, 2, 3, 4, 5]
+# y_values = [1, 4, 9, 16, 25]
+
+# plt.style.use('seaborn')
+# fig, ax = plt.subplots()
+# ax.scatter(x_values, y_values, s=100)
+
+
+# =============================================================================
+
+x_values = range(1, 1001)
 y_values = [x**2 for x in x_values]
 
-plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=40)
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c='red', s=10)
+ax.axis([0, 1100, 0, 1100000])
 
-# Set chart title, and label axes.
-plt.title("Square Numbers", fontsize=24)
-plt.xlabel("Value", fontsize=14)
-plt.ylabel("Square of Value", fontsize=14)
+plt.savefig('squares_plot.png', bbox_inches='tight')
 
-# Set size of tick labels.
-plt.tick_params(axis='both', which='major', labelsize=14)
 
-# Set the range for each axis.
-plt.axis([0, 1100, 0, 1100000])
 
-plt.show()
+
+# =============================================================================
+
+
+# =============================================================================
+
+# x_values = list(range(1, 1001))
+# y_values = [x**2 for x in x_values]
+
+# plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolor='none', s=40)
+
+# # Set chart title, and label axes.
+# plt.title("Square Numbers", fontsize=24)
+# plt.xlabel("Value", fontsize=14)
+# plt.ylabel("Square of Value", fontsize=14)
+
+# # Set size of tick labels.
+# plt.tick_params(axis='both', which='major', labelsize=14)
+
+# # Set the range for each axis.
+# plt.axis([0, 1100, 0, 1100000])
+
+# plt.show()
